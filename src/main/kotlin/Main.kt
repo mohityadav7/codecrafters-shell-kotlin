75785916a9cd3b552ba2122f862ac069fb4e5a1a
Mohit Yadav <mohit@mbook.local>
2024-12-28 16:43:58 +0530

    add exit funcationality

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,19 @@
+import kotlin.system.exitProcess
+
 fun main() {
     while (true) {
         print("$ ")
-        val command = readln()
-        print("$command: command not found\n")
+        val line = readln()
+        val words = line.split(" ")
+        if (words.isNotEmpty()) {
+            if (words.first() == "exit") {
+                if (words.size > 1) {
+                    exitProcess(words[1].toInt())
+                } else {
+                    exitProcess(0)
+                }
+            }
+        }
+        println("${words[0]}: command not found")
     }
 }
