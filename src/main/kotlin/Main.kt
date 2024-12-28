45a929f8590c804dc194da865bbea52e84ac2ab5
Mohit Yadav <mohit@mbook.local>
2024-12-28 23:51:47 +0530

    refactor code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,84 +5,93 @@ import java.nio.file.Path
 import kotlin.io.path.exists
 import kotlin.system.exitProcess
 
-val builtinCommands = listOf("exit", "echo", "type")
-
 fun main() {
     while (true) {
         print("$ ")
         val line = readln()
-        val inputArgs = line.split(" ")
+        handleCommand(line)
+    }
+}
 
-        var command: String
-        try {
-            command = inputArgs.first()
-        } catch (e: NoSuchElementException) {
-            continue
+fun handleCommand(inputLine: String) {
+    val (cmd, args) = inputLine.trim().split("\\s+".toRegex()).let {
+        if (it.isEmpty()) return
+        Command.from(it.first()) to it.drop(1)
+    }
+
+    when (cmd) {
+        is Command.Exit -> {
+            var statusCode = 0
+            try {
+                if (args.isNotEmpty()) statusCode = args[0].toInt()
+            } catch (_: Exception) { }
+            exitProcess(statusCode)
         }
 
-        when (command) {
-            "exit" -> {
-                var statusCode = 0
-                if (inputArgs.size > 1) {
-                    try {
-                        statusCode = inputArgs[1].toInt()
-                    } catch (_: Exception) { }
-                }
-                exitProcess(statusCode)
-            }
+        is Command.Echo -> {
+            println(inputLine.substringAfter("${cmd.value} ", ""))
+        }
 
-            "echo" -> {
-                val echoString = inputArgs.subList(1, inputArgs.size).joinToString(separator = " ")
-                println(echoString)
-            }
-
-            "type" -> {
-                val pathFiles = System.getenv("PATH")
-                    .split(":")
-                    .filter { pathString ->
-                        Path.of(pathString).exists()
-                    }
-                    .flatMap { dirPath ->
-                        File(dirPath).listFiles()?.toList() ?: listOf()
-                    }
-                inputArgs.subList(1, inputArgs.size).forEach { inputArg ->
-                    if (inputArg in builtinCommands) {
-                        println("$inputArg is a shell builtin")
-                    } else {
-                        pathFiles.firstOrNull { it.name == inputArg }?.let {
-                            println("${it.name} is ${it.path}")
-                        } ?: run {
-                            println("$inputArg: not found")
-                        }
-                    }
-                }
-            }
-
-            else -> {
-                System.getenv("PATH")
-                    .split(":")
-                    .filter { pathString ->
-                        Path.of(pathString).exists()
-                    }
-                    .flatMap { dirPath ->
-                        File(dirPath).listFiles()?.toList() ?: listOf()
-                    }.firstOrNull { it.name == command }?.let {
-                        try {
-                            val process = ProcessBuilder(inputArgs)
-                                .redirectErrorStream(true)
-                                .start()
-                            val reader = BufferedReader(InputStreamReader(process.inputStream))
-                            reader.lines().forEach {
-                                println(it)
-                            }
-                            process.waitFor()
-                        } catch (e: Exception) {
-                            e.printStackTrace()
-                        }
+        is Command.Type -> {
+            args.forEach { arg ->
+                if (arg in Command.entries.map { it.value }) {
+                    println("$arg is a shell builtin")
+                } else {
+                    findExecutablePath(arg)?.let {
+                        println("$arg is $it")
                     } ?: run {
-                    println("$command: command not found")
+                        println("$arg: not found")
+                    }
                 }
             }
         }
+
+        is Command.None -> {
+            println()
+        }
+
+        is Command.Unknown -> {
+            val cmdPath = findExecutablePath(cmd.value)
+            cmdPath?.let {
+                try {
+                    val process = ProcessBuilder(cmdPath, *args.toTypedArray())
+                        .redirectErrorStream(true)
+                        .start()
+                    val reader = BufferedReader(InputStreamReader(process.inputStream))
+                    reader.lines().forEach {
+                        println(it)
+                    }
+                    process.waitFor()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            } ?: run {
+                println("${cmd.value}: command not found")
+            }
+        }
+    }
+}
+
+fun findExecutablePath(executable: String): String? {
+    return System.getenv("PATH")
+        .split(":")
+        .filter { Path.of(it).exists() }
+        .flatMap { dirPath -> File(dirPath).listFiles()?.toList() ?: listOf() }
+        .firstOrNull { it.name == executable }?.path
+}
+
+sealed class Command(val value: String) {
+    data object None : Command("")
+    data object Echo: Command("echo")
+    data object Type: Command("type")
+    data object Exit: Command("exit")
+    class Unknown(value: String): Command(value)
+
+    companion object {
+        val entries: List<Command> =
+            Command::class.sealedSubclasses.mapNotNull { it.objectInstance }
+
+        fun from(value: String): Command =
+            entries.firstOrNull { it.value == value } ?: Unknown(value)
     }
 }
